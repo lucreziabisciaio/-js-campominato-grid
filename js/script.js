d1968d208090328a550e141f089e74f3e4bbe4da
lucreziabisciaio <lucreziabisciaio@gmail.com>
2021-12-17 16:12:49 +0100

    created file and added js instructions

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//creo 3 pulsanti
+//inserisco le funzioni all'interno dell'addEvent
+//i campi si generano al click dell'utente
+//con append aggiungo la classe del background alla generica del box
+//torno al background del generico con toggle
