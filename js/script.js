92148afd34ec281e4ae8c53618a03c7f9b9bb033
lucreziabisciaio <lucreziabisciaio@gmail.com>
2021-12-17 16:36:00 +0100

    level-1 function (add 100 box)

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@
 const button1 = document.querySelector('.level_1');
 const boxContainer = document.querySelector('.box_container');
 
+button1.addEventListener('click', function () {
+
+    function createBox(container) {
+        const box = document.createElement('div');
+        box.className = 'box';
+        container.append(box);
+    }
+
+    for (let i = 0; i < 100; i++) {
+        createBox(boxContainer);
+    }
+
+})
